01c25c7e44eb8fa32bda7bfad9f13d8a601a0c95
Seokhun-Yang <ddayang1234@gmail.com>
2021-12-07 23:32:18 +0700

    Comment added

--- src/UsingString.java
+++ src/UsingString.java
@@ -2,7 +2,7 @@ public class UsingString {
     public static void main(String[] args) {
         String a = "hello";
         String b = new String("hello");
-        System.out.println(a.equals(b));
-        System.out.println(a == b);
+        System.out.println(a.equals(b)); //true
+        System.out.println(a == b); //false
     }
 }
