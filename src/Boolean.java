56f11047b0392786515488734310b6e7f0bbee4f
Seokhun-Yang <ddayang1234@gmail.com>
2021-12-07 23:21:46 +0700

    Initial commit

---
+++ src/Boolean.java
@@ -0,0 +1,14 @@
+public class Boolean {
+    public static void main(String[] args) {
+        int base = 180;
+        int height = 185;
+        boolean isTall = height > base;
+
+        if (isTall) {
+            System.out.println("키가 크다.");
+        }
+        else {
+            System.out.println("키가 작다");
+        }
+    }
+}
---
+++ src/Char.java
@@ -0,0 +1,11 @@
+public class Char {
+    public static void main(String[] args) {
+        char a = 'a'; //문자
+        char b = 97; //아스키코드
+        char c = '\u0061'; //유니코드
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
